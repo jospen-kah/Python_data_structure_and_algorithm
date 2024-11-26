9cadc96a02494daf3b92947490c3be3d79833c8a
jospen-kah <ngumkahjospen@gmail.com>
2024-11-26 11:24:02 +0100

    second commit

--- Day 1/hello.py
+++ Day 1/hello.py
@@ -1,1 +1,11 @@
 print('Hello World!!')
+
+a = int(input("Enter  the first number a: "))
+b = int(input("Enter the second number b: "))
+
+sum = a + b
+multiplication = a * b
+print(f"the sum is of {a} and {b} is {sum}" )
+print(f"the product is of {a} and {b} is {multiplication}" )
+
+
